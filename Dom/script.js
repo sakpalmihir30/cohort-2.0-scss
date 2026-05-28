0caba47aa45fa831607d8ab86cca05cbc89daa07
user.name : "Mihir Sakpal <sakpalmihir30@gmail.com>
2026-05-28 13:38:30 +0530

    card

---
+++ Dom/script.js
@@ -0,0 +1,27 @@
+var grow = 0
+var btn = document.querySelector('button');
+var h2 = document.querySelector('h2');
+var inner = document.querySelector('.inner')
+
+btn.addEventListener('click', function () {
+
+    btn.style.pointerEvents = 'none';
+    var num = 50 + Math.floor(Math.random() * 50);
+
+    var int = setInterval(() => {
+
+        grow++;
+        h2.innerHTML = grow + '%';
+        inner.style.width = grow + '%';
+    }, num);
+
+    setTimeout(() => {
+        clearInterval(int);
+        btn.innerHTML = 'DOWNLOADED';
+        btn.style.opacity = 0.5;
+        console.log('Downloaded in just', num / 10, ' secound');
+    }, num * 100);
+
+
+
+})
